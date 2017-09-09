f0bc2df98c0191bb7e51bb7288984544ba2e5a36
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-09-09 09:38:51 +0100

    update run sequence for life on Heroku :)

--- run.py
+++ run.py
@@ -5,10 +5,23 @@ from api.utils.factory import create_app
 from api.utils.config import DevelopmentConfig, ProductionConfig
 import os
 
+
+use_config = {
+    'PROD': ProductionConfig,
+    'DEV': DevelopmentConfig,
+}
+use_reloader = {
+    'PROD': True
+}
+
+config = use_config.get(
+    os.environ.get('WORK_ENV', 'DEV')
+)
+
+app = create_app(config)
+
 if __name__ == '__main__':
-    if os.environ.get('WORK_ENV') == 'PROD':
-        app = create_app(ProductionConfig)
-        app.run(port=5000, host="0.0.0.0", use_reloader=False)
-    else:
-        app = create_app(DevelopmentConfig)
-        app.run(port=5000, host="0.0.0.0", use_reloader=True)
+    use_reloader = use_reloader.get(
+        os.environ.get('WORK_ENV', 'DEV')
+    )
+    app.run(port=5000, host="0.0.0.0", use_reloader=use_reloader)
